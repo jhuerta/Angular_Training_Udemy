d9fc5b204d5423efb40cf3f2c853771a517c281e
Juan Huerta <juan.huerta@gmail.com>
2018-11-14 11:35:13 +0100

    First assignment completed

---
+++ assignment_1/src/app/warning-error/warning-error.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[app-warning-error]',
+  template: '<h1>Error, error, error!</h1>',
+  styles: ['h1{color: red;}']
+})
+export class WarningErrorComponent {
+}
