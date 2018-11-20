6468442863a26d91881803b1d2044b6560ce9603
Juan Huerta <juan.huerta@gmail.com>
2018-11-21 00:26:36 +0100

    My solution

---
+++ forms_reactive_assignment/src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+    statuses: string[] = ["Stable", "Critical", "Finished"];
+    signupForm: FormGroup = null;
+
+    onSubmit() {
+        console.log(this.signupForm.value);
+    }
+
+    ngOnInit() {
+        this.signupForm = new FormGroup({
+            project_name: new FormControl("female", [
+                this.noTestName,
+                Validators.required
+            ]),
+            mail: new FormControl(
+                "juan@gmail.com",
+                [Validators.required, Validators.email],
+                this.forbiddenEmails
+            ),
+            project_status: new FormControl("Finished")
+        });
+    }
+
+    noTestName(control: FormControl): { [s: string]: boolean } {
+        if (control.value === "test") {
+            return { noTestNameAllowed: true };
+        }
+        return null; // Successful validation
+    }
+
+    forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+        const promise = new Promise<any>((resolve, reject) => {
+            setTimeout(() => {
+                if (control.value === "test@test.com") {
+                    resolve({ emailIsForbidden: true });
+                } else {
+                    resolve(null);
+                }
+            }, 100);
+        });
+
+        return promise;
+    }
+}
