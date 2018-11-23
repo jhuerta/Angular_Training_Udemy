467203df9bc5945f058e659ae586acb996ff4a4d
Juan Huerto <juan.huerta@gmail.com>
2018-11-23 17:01:23 +0100

    Keyframes and group animations

--- animations_lesson_26/src/app/app.component.ts
+++ animations_lesson_26/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {animate, trigger, state, style, transition} from '@angular/animations';
+import {animate, keyframes, group, trigger, state, style, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -33,9 +33,36 @@ import {animate, trigger, state, style, transition} from '@angular/animations';
   	[
 	  	state('in',style({'opacity':1, transform:'translateX(0) translateY(0)'})),
 	  	transition('void => *',  [style({'opacity':0,transform:'translateX(-100px) translateY(-50px)'}), animate(1500)]),
-	  	transition('* => void', animate(500,style({transform:'translateX(100px)','opacity':0})),
+	  	transition(		'* => void', 
+	  					group([
+	  					animate(1000,style({color:'yellow'})),
+	  					animate(1000,style({transform:'translateX(100px)','opacity':0}))
+	  					])
+
+	  					),
   	],
-  	) 
+  	) ,
+
+
+  trigger('list2',
+  	[
+	  	state('in',style({'opacity':1, transform:'translateX(0) translateY(0)'})),
+	  	transition('void => *',  [ animate(1000, keyframes([
+	  		style({transform: 'translateX(-100px)', 'opacity': 0, 'offset':0}),
+			style({transform: 'translateX(-50px)', 'opacity': 0.1, 'offset':0.8}),
+			style({transform: 'translateX(-10px)', 'opacity': 0.2, 'offset':0.9}),
+			style({transform: 'translateX(0)', 'opacity': 1, 'offset':1})
+	  		])) ]),
+	  	transition(	'* => void', 
+  					[
+  					animate(500,style({color:'red'})),
+  					animate(500,style({transform:'translateX(100px)','opacity':0}))
+  					]
+  					),
+  	],
+  	)
+
+
 
   ]
 })
