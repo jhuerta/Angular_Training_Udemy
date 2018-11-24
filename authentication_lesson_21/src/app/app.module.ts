941fb3c0ddf06017b2aab00b71ac28779ce787fc
Juan Huerta <juan.huerta@gmail.com>
2018-11-24 23:13:29 +0100

    Extraced all app in nice modules. Use CORE module, it is handy and clena

--- authentication_lesson_21/src/app/app.module.ts
+++ authentication_lesson_21/src/app/app.module.ts
@@ -1,38 +1,25 @@
+import { CoreModule } from "./core/core.module";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpModule } from "@angular/http";
 import { SharedModule } from "./shared/shared.module";
 
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { AppRoutingModule } from "./app-routing.module";
-import { DataStorageService } from "./shared/data-storage.service";
-import { AuthService } from "./auth/auth.service";
-import { RecipeService } from "./recipes/recipe.service";
-// import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
+
 import { AuthModule } from "./auth/auth.module";
 
-import { IgnoredServices } from "./credentials.services.ngsec";
-import { HomeComponent } from "./home/home.component";
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent],
   imports: [
     SharedModule,
-    // RecipesModule,
     ShoppingListModule,
     AuthModule,
     BrowserModule,
     HttpModule,
-    AppRoutingModule
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    DataStorageService,
-    AuthService,
-    IgnoredServices
+    AppRoutingModule,
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
